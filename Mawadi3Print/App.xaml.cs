cdeb0cf4202eb9a1357761f1263754032fe08545
7Xme <ayoubatigui33@gmail.com>
2026-07-24 18:55:50 +0100

    up

--- Mawadi3Print/App.xaml.cs
+++ Mawadi3Print/App.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Threading;
 
 namespace Mawadi3Print
 {
@@ -6,7 +7,29 @@ namespace Mawadi3Print
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += OnAppDomainUnhandledException;
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
             base.OnStartup(e);
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "خطأ غير متوقع", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+
+        private void OnAppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception)?.ToString() ?? "Unknown error",
+                "خطأ غير متوقع", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void OnUnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception?.ToString() ?? "Task error",
+                "خطأ مهمة غير متوقع", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.SetObserved();
         }
     }
 }
